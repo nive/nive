1a09af3176fc07eaba86593aef2dec4a9026e588
adroullier <arndt@nive.co>
2013-10-24 18:08:46 +0200

    added jaon exporter

---
+++ nive/tools/exportJson.py
@@ -0,0 +1,113 @@
+# Copyright 2012, 2013 Arndt Droullier, Nive GmbH. All rights reserved.
+# Released under GPL3. See license.txt
+#
+
+import json
+import base64
+
+from nive.tool import Tool, ToolView
+from nive.definitions import ToolConf, FieldConf, ViewConf
+from nive.definitions import IApplication, MetaTbl, Structure, IContainer
+from nive.helper import ConfEncoder
+from nive.i18n import _
+
+configuration = ToolConf(
+    id = "exportJson",
+    context = "nive.tools.exportJson.exportJson",
+    name = _(u"Json data export"),
+    description = _("This function exports all objects in json format. Optionally as flat list or tree structure. This is not a simple database table dump. Object events will be triggered before exporting data."),
+    apply = (IApplication,),
+    mimetype = "text/json",
+    data = [
+        FieldConf(id="tree", 
+                  datatype="bool", 
+                  default=1, 
+                  listItems=[{"id":"true", "name":"Tree"},{"id":"false","name":"Flat list"}], 
+                  name=_(u"Export as tree"),
+                  description=_(u"Export objects as tree structure (contained objects are included as 'items')")),
+        FieldConf(id="filedata", 
+                  datatype="radio", 
+                  default="none", 
+                  listItems=[{"id":"none", "name":"Only file information"},
+                             {"id":"data", "name":"Include file data"},
+                             {"id":"path","name":"No file data, only filenames and local paths"}], 
+                  name=_(u"File data"),
+                  description=_(u"Include binary file data in json export (encoded as base64)"))
+    ],
+    views = [
+        ViewConf(name="", view=ToolView, attr="form", permission="system", context="nive.tools.exportJson.exportJson"),
+    ]
+)
+
+class exportJson(Tool):
+    """
+    """
+
+    def _Run(self, **values):
+
+        result = 1
+        codepage="utf-8"
+    
+        app = self.app
+        datapool = app.db
+        conf = app.dbConfiguration
+        conn = datapool.connection
+        tree = values.get("tree")
+        filedata = values.get("filedata")
+        self.filename = app.configuration.id + ".json"
+
+        if not conn:
+            self.stream.write(_(u"Database connection error (${name})\n", mapping={u"name": app.poolTag}))
+            return 0
+        
+        if not conn.IsConnected():
+            self.stream.write(_(u"Database connection error (${name})\n", mapping={u"name": app.poolTag}))
+            return 0
+        
+        def mapfields(fields):
+            return [f.id for f in fields]
+        
+        metaflds = mapfields(app.GetAllMetaFlds(ignoreSystem=False))
+
+        def exportObj(o):
+            values = {"__items__": []}
+            for field in metaflds:
+                values[field] = o.meta.get(field)
+            for field in o.configuration.data:
+                if field["datatype"] == "file":
+                    file = o.files.get(field.id)
+                    if not file:
+                        continue
+                    values[field.id] = {"filename": file.filename, "size": file.size}
+                    if filedata=="path":
+                        values[field.id]["path"] = file.path
+                    elif filedata=="data":
+                        values[field.id]["data"] = base64.b64encode(file.read())
+                else:
+                    values[field.id] = o.data.get(field.id)
+            
+            if IContainer.providedBy(o):
+                for child in o.GetObjs():
+                    cv = exportObj(child)
+                    values["__items__"].append(cv)
+            return values
+                
+        root = app.root()
+        data = {"__items__": []}
+        for child in root.GetObjs():
+            cv = exportObj(child)
+            data["__items__"].append(cv)
+            
+        if not tree:
+            def getlist(values, datalist):
+                for v in values["__items__"]:
+                    datalist = getlist(v, datalist)
+                del values["__items__"]
+                datalist.append(values)
+                return datalist
+            data = getlist(data, [])
+            
+        self.stream.write(ConfEncoder().encode(data))
+        
+        return 1
+
---
+++ nive/tools/tests/test_exportJson.py
@@ -0,0 +1,62 @@
+
+import time
+import unittest
+
+from nive.definitions import *
+from nive.tools.exportJson import *
+
+from nive.tests import db_app
+
+from nive.helper import FormatConfTestFailure
+from nive.security import User
+
+# -----------------------------------------------------------------
+
+class DBExportTest1(unittest.TestCase):
+
+    def test_conf1(self):
+        r=configuration.test()
+        if not r:
+            return
+        print FormatConfTestFailure(r)
+        self.assert_(False, "Configuration Error")
+
+    def test_tool(self):
+        exportJson(configuration,None)
+        
+    
+class DBExportTest1_db(unittest.TestCase):
+
+    def setUp(self):
+        self.app = db_app.app_db()
+        self.app.Register(configuration)
+        root = self.app.root()
+        self.o=o=db_app.createObj1(root)
+        db_app.createObj1(o)
+        db_app.createObj2(o)
+        db_app.createObj3(o)
+
+    def tearDown(self):
+        self.app.root().Delete(self.o.id, user=User("aaa"))
+        self.app.Close()
+    
+    def test_toolrun1(self):
+        t = self.app.GetTool("nive.tools.exportJson", self.app)
+        self.assert_(t)
+        r,v = t(tree=1,filedata="none")
+        self.assert_(r)
+
+
+    def test_toolrun2(self):
+        t = self.app.GetTool("exportJson", self.app)
+        self.assert_(t)
+        r,v = t(tree=0,filedata="path")
+        self.assert_(r)
+
+
+    def test_toolrun3(self):
+        t = self.app.GetTool("exportJson", self.app)
+        self.assert_(t)
+        r,v = t(tree=1,filedata="data")
+        self.assert_(r)
+
