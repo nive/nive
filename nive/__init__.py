47cff4fe1751f65deaa6a7e92f7ba4c79fe668a7
adroullier <arndt@nive.co>
2014-01-23 13:12:01 +0100

    added imports

--- nive/__init__.py
+++ nive/__init__.py
@@ -10,3 +10,5 @@ Use `nive.definitions` to import Interfaces or Configuration classes
 """
 from nive.utils.dataPool2.files import File
 from nive.utils.dataPool2.base import FileNotFound
+from nive.views import Unauthorized
+from nive.views import Mail
