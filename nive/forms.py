42ffd669e3ca5a67107cda910f571c63a300055c
adroullier <arndt@nive.co>
2015-02-25 19:24:12 +0100

    fixed app lookup if context is None

--- nive/forms.py
+++ nive/forms.py
@@ -281,7 +281,7 @@ class Form(Events, ReForm):
         self.view = view
         self.context = context or view.context if view else None
         self.request = request or view.request if view else None
-        self.app = app or self.context.app
+        self.app = app or self.context.app if self.context else None
         
         if loadFromType:
             self.loadFromType = loadFromType
