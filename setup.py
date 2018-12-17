a4560eff1c5a44e3b915081782e4697391486b9d
adroullier <arndt@nive.co>
2018-12-17 20:00:02 +0100

    new version 1.0.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requires = [
 
 setupkw = dict(
       name='nive',
-      version='1.0.3',
+      version='1.0.4',
       description='Nive base package',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
