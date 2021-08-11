989ba51d9ebb8367a76dce0e46f453bb1a68add5
adroullier <arndt@nive.co>
2021-08-11 16:01:42 +0200

    removed RetryServiceRequest error view

--- nive/components/iface/portal.py
+++ nive/components/iface/portal.py
@@ -4,31 +4,27 @@ import traceback
 
 from pyramid.exceptions import Forbidden
 from pyramid.httpexceptions import HTTPError
-from pyramid.renderers import render_to_response, render
+from pyramid.renderers import render_to_response
 
 from nive import portal
 
 
 class IfacePortal(portal.Portal):
 
-
+    # TODO remove default views
     def SetupPortalViews(self, config):
         # redirects
         config.add_view(servererror, context=Exception)
-        config.add_view(retry, context=RetryServiceRequest)
         config.add_view(httperror, context=HTTPError)
         config.add_view(portal.forbidden_view, context=Forbidden)
         config.add_view(portal.portal_view, name="", context="nive.portal.Portal")
-        config.add_view(portal.robots_view, name="robots.txt", context="nive.portal.Portal")
-        config.add_view(portal.sitemap_view, name="sitemap.xml", context="nive.portal.Portal")
         config.add_view(portal.logout_view, name="logout", context="nive.portal.Portal")
         config.add_view(portal.login_view,  name="login", context="nive.portal.Portal")
         config.add_view(portal.account_view,name="account", context="nive.portal.Portal")
+        config.add_view(portal.robots_view, name="robots.txt", context="nive.portal.Portal")
+        config.add_view(portal.sitemap_view, name="sitemap.xml", context="nive.portal.Portal")
         #config.add_view(favicon_view, name="favicon.txt", context="nive.portal.Portal", view=PortalViews)
     
-        # translations
-        #config.add_translation_dirs('nive:locale/')
-        
         config.commit()
 
 
@@ -81,31 +77,3 @@ def httperror(errorResponse, request):
     resp.status = "404 Not found"
     return resp
  
-def retry(excp, request):
-    """
-    Called in case a service exists but is not ready yet
-    """
-    errorResponse = render_to_response("nive.components.iface:templates/error.pt",
-                              {"error":"503", "msg":str(excp)},
-                              request=request)
-    errorResponse.status = "503 Service temporarily unavailable"
-    headers = [("Retry-After", str(excp))]
-    if hasattr(errorResponse, "headerlist"):
-        headers += list(errorResponse.headerlist)
-    errorResponse.headerlist = headers
-    if request.environ.get("HTTP_X_REQUESTED_WITH")=="XMLHttpRequest":
-        return errorResponse
-    message = render("peak.engine.views:retry.pt",
-                     {"error":"503", "msg":str(excp)},
-                     request=request)
-    errorResponse.unicode_body = message
-    return errorResponse
-
-
-class RetryServiceRequest(HTTPError): #KeyError
-    """
-    Used if uncached service is hit
-    """
-    code = 503
-    title = 'Service Unavailable'
-    explanation = 'The server is currently unavailable. Please try again at a later time.'
